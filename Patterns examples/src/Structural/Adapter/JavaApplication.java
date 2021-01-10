3353bc5bb58dc7230306f524ccbf21eb975ba081
Ivan Aldabaev <Aldabaevivan1985@gmail.com>
2021-01-10 13:52:21 +0100

    Add Prototype

---
+++ Patterns examples/src/Structural/Adapter/JavaApplication.java
@@ -0,0 +1,16 @@
+package Structural.Adapter;
+
+public class JavaApplication {
+    public  void saveObject(){
+        System.out.println("Saving java object");
+    }
+    public void updateObject(){
+        System.out.println("Updating java object");
+    }
+    public void loadObject(){
+        System.out.println("Loading java object");
+    }
+    public void  deleteObject(){
+        System.out.println("Deleting java object");
+    }
+}
