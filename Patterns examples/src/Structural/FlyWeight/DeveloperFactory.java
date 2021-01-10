cf607adb4bbb90700d95933ee7eacb444d3c2fa0
Ivan Aldabaev <Aldabaevivan1985@gmail.com>
2021-01-10 20:08:53 +0100

    Add FlyWeight

---
+++ Patterns examples/src/Structural/FlyWeight/DeveloperFactory.java
@@ -0,0 +1,28 @@
+package Structural.FlyWeight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DeveloperFactory {
+    private static final Map<String,Developer> developers = new HashMap<>();
+
+    public Developer getDeveloperBySpecialty(String specialty){
+       Developer developer = developers.get(specialty);
+       if(developer==null){
+           switch (specialty) {
+               case "java" -> {
+                   System.out.println("Hiring Java developer...");
+                   developer = new JavaDeveloper();
+               }
+               case "c++" -> {
+                   System.out.println("Hiring C++ developer...");
+                   developer = new CppDeveloper();
+               }
+               default -> throw new IllegalStateException("Unexpected value: " + specialty);
+           }
+       developers.put(specialty,developer);
+
+        }
+        return  developer;
+    }
+}
---
+++ Patterns examples/src/Structural/FlyWeight/ProjectRunner.java
@@ -0,0 +1,24 @@
+package Structural.FlyWeight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectRunner {
+    public static void main(String[] args) {
+        DeveloperFactory factory = new DeveloperFactory();
+        List<Developer> developers = new ArrayList<>();
+        developers.add(factory.getDeveloperBySpecialty("java"));
+        developers.add(factory.getDeveloperBySpecialty("java"));
+        developers.add(factory.getDeveloperBySpecialty("java"));
+        developers.add(factory.getDeveloperBySpecialty("java"));
+        developers.add(factory.getDeveloperBySpecialty("java"));
+        developers.add(factory.getDeveloperBySpecialty("c++"));
+        developers.add(factory.getDeveloperBySpecialty("c++"));
+        developers.add(factory.getDeveloperBySpecialty("c++"));
+        developers.add(factory.getDeveloperBySpecialty("c++"));
+
+        for (Developer developer: developers){
+            developer.writeCode();
+        }
+    }
+}
