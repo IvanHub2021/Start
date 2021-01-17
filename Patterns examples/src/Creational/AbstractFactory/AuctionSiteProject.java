dfaf96a2f3ca24dad2f735cd28ac43327da8440e
Ivan Aldabaev <Aldabaevivan1985@gmail.com>
2021-01-17 18:28:41 +0100

    Add Chain Of Responsibility

--- Patterns examples/src/Creational/AbstractFactory/AuctionSiteProject.java
+++ Patterns examples/src/Creational/AbstractFactory/AuctionSiteProject.java
@@ -6,7 +6,7 @@ import Creational.AbstractFactory.website.WebSiteTeamFactory;
 public class AuctionSiteProject {
     public static void main(String[] args) {
         ProjectTeamFactory projectTeamFactory = new WebSiteTeamFactory();
-        Developer developer=projectTeamFactory.getDeveloper();
+        Developer developer = projectTeamFactory.getDeveloper();
         Tester tester = projectTeamFactory.getTester();
         ProjectManager projectManager = projectTeamFactory.getProjectManager();
         System.out.println("Creating banking system...\n");
